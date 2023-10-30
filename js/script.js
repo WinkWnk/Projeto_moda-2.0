e1d2d76ec3926c294e0dce8a470d432ff1cc3042
Werneck <werneckcolins@outlook.com>
2023-10-30 20:56:34 -0300

    primeira manutenção

--- js/script.js
+++ js/script.js
@@ -2,6 +2,19 @@ let email = document.querySelector("#email")
 let confirmacao = document.querySelector("#confirmar")
 //Tornando o conteúdo do email minúsculo 
 
+// Validação campo nome
+let nome = document.querySelector("#nome")
+
+function exibirNome(){
+    if(nome.value == ""){
+        nome.style.border = "2px solid red"
+    }
+    else{
+        nome.style.border = "2px solid green"
+    }
+}
+nome.addEventListener("blur", exibirNome)
+
 email.addEventListener("keyup", function () {
     let minusculo = email.value.toLowerCase()
     confirmacao.value = email.value
@@ -41,3 +54,26 @@ btn_olho.addEventListener("click", () => {
         senha.setAttribute("type", "password")
     }
 })
+
+function validarFormulario(){
+
+    if (nome.value === '' || equipe.value === ''|| placa.value === '' || modelo.value === ''|| local.value === ''){
+        return false;   
+    }
+    else{
+        return true    
+    }
+    };
+ 
+    document.getElementById("btnEnviar").addEventListener("click", (evento)=>{
+        evento.preventDefault()
+        if(validarFormulario()){
+            salvarDados()
+            window.location.href="index.html"
+        }
+        else{
+            alert("Por favor preencha todos os campos.");
+        }
+        
+      });
+    
